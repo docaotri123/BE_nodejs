e9ac8799c2a141765ab56fb0874697ddf15686a7
unknown <docaotri211997@gmail.com>
2019-11-04 20:28:01 +0700

    test

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { Controllers } from './controller/AllController';
 
 const app = createExpressServer({
     defaultErrorHandler: false,
+    cors: true,
     controllers: Controllers,
     middlewares: []
 });
