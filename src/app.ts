3e94524e9b5ddcaebfb67379575c3f617f0de882
2359 <media2359@KhaMBA.local>
2020-01-31 11:14:01 +0700

    remove swagger

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,6 @@ import { createConnection } from 'typeorm';
 import { BookRoomService } from './service/v1.0/BookRoomService';
 import { listenToBookingQueue } from './job_queue/worker';
 import { SERVER_PORT, sqlConfig, appConfig } from './app.config';
-import { optionsSwagger } from './swagger/swagger.config';
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
 
 console.log('connect SQL');
 createConnection(sqlConfig)
@@ -19,14 +16,9 @@ createConnection(sqlConfig)
     .catch(err => {
         console.log('Error while connecting to the database', err);
         return err;
-    })
+    });
 
 const app = createExpressServer(appConfig);
-var options = {
-    explorer: true
-  };
-const swaggerSpec = swaggerJsdoc(optionsSwagger);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, options));
 
 app.listen(SERVER_PORT, () => console.log(`Server is running port ${SERVER_PORT}`));
 
